007afe91ea4ea5f30a7dd0e2b415ae11671b5889
ansfatikhaa <fatikhaanisatul@gmail.com>
2024-04-04 14:16:35 +0700

    table atk

--- miniproject-atk/src/components/atks/ListAtkComponent.jsx
+++ miniproject-atk/src/components/atks/ListAtkComponent.jsx
@@ -6,20 +6,15 @@ function ListAtkComponent() {
     const [atks, setAtks] = useState([]);
     const navigator = useNavigate();
 
-    // useEffect(() => {
-    //     listAtks()
-    //         .then(response => {
-    //             console.log(response); // Tambahkan ini untuk mengetahui struktur respons
-    //             if (response.data && Array.isArray(response.data)) {
-    //                 setAtks(response.data);
-    //             } else {
-    //                 console.error('Response data is not in expected format:', response);
-    //             }
-    //         })
-    //         .catch(error => {
-    //             console.error(error);
-    //         });
-    // }, []);
+    useEffect(() => {
+        listAtks()
+        .then((response) => {
+            setAtks(response.data.data);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+    }, []);
 
     function addNewAtk() {
         navigator('/add-atk');
@@ -35,7 +30,7 @@ function ListAtkComponent() {
                 </div>
                 <div className="card-body">
                     <div className="table-responsive">
-                        <table className="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <table className="table table-bordered" id="dataTable" width="100%" cellSpacing="0">
                             <thead>
                                 <tr>
                                     <th>Nama</th>
@@ -47,29 +42,15 @@ function ListAtkComponent() {
                                 </tr>
                             </thead>
                             <tbody>
-                                {/* {atks.map(atk => (
+                                {atks.map(atk => (
                                     <tr key={atk.id}>
                                         <td>{atk.nama}</td>
                                         <td>{atk.harga}</td>
                                         <td>{atk.stok}</td>
-                                        <td>{atk.supplier}</td>
-                                        <td>{atk.status === '1' ? 'Aktif' : 'Tidak Aktif'}</td>
+                                        <td>{atk.sup}</td>
+                                        <td>{atk.status == 1 ? 'Aktif' : 'Tidak Aktif'}</td>
                                     </tr>
-                                ))} */}
-                                <tr>
-                                    <td>Lion Nixon</td>
-                                    <td>$320,800</td>
-                                    <td>5</td>
-                                    <td>61</td>
-                                    <td>Aktif</td>
-                                </tr>
-                                <tr>
-                                    <td>Tiger Nixon</td>
-                                    <td>$340,800</td>
-                                    <td>5</td>
-                                    <td>61</td>
-                                    <td>Aktif</td>
-                                </tr>
+                                ))}
                             </tbody>
                         </table>
                     </div>
--- miniproject-atk/src/service/AtkService.js
+++ miniproject-atk/src/service/AtkService.js
@@ -1,6 +1,6 @@
 // AtkService.js
 import axios from "axios";
 
-const REST_API_BASE_URL = "http://localhost:8080/atks";
+const REST_API_BASE_URL = "http://10.1.6.160:8080/atks";
 
 export const listAtks = () => axios.get(REST_API_BASE_URL + "/getAtks");
